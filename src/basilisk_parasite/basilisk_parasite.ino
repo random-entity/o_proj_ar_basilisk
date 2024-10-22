d5df95452742cabb8ec03adcd877ba8d8db6c807
random-entity <randomentityemail@gmail.com>
2024-10-23 03:25:44 +0900

    Turn Teensy source files directory into an Arduino library to use it for tests

--- src/basilisk_parasite/basilisk_parasite.ino
+++ src/basilisk_parasite/basilisk_parasite.ino
@@ -1,15 +1,5 @@
 #include <Arduino.h>
-
-#include "basilisk.h"
-#include "cmd_rcvrs/neokey_cr.h"
-#include "components/neokey.h"
-#include "components/specifics/neokey1x4_i2c0.h"
-#include "executer.h"
-#include "globals/serials.h"
-#include "helpers/serial_print.h"
-#include "rpl_sndrs/led_rs.h"
-#include "rpl_sndrs/serial_rs.h"
-// #include "globals/timing.h"
+#include <Basilisk.h>
 
 // Basilisk configuration overrides.
 Basilisk::Configuration cfg{
---
+++ src/basilisk_teensy_library/Basilisk.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Arduino.h>
+
+#include "basilisk.h"
+#include "cmd_rcvrs/neokey_cr.h"
+#include "components/neokey.h"
+#include "components/specifics/neokey1x4_i2c0.h"
+#include "executer.h"
+#include "globals/serials.h"
+#include "helpers/serial_print.h"
+#include "rpl_sndrs/led_rs.h"
+#include "rpl_sndrs/serial_rs.h"
+// #include "globals/timing.h"
